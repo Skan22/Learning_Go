f6b7a8959d23d4e353b7933968181b294d44b881
Skan22 <skanderyacoubi2005@gmail.com>
2026-04-29 14:23:58 +0100

    port scanner finished

--- PortScanner/toofastscanner.go
+++ PortScanner/toofastscanner.go
@@ -1,26 +1,39 @@
 package main
 
 import (
+	
 	"fmt"
 	"net"
-	"strconv"
+
 	"sync"
 )
 
-func main()  {
-	var wg sync.WaitGroup
-	for i:=0;i<=65535;i++{	
-		wg.Add(1)
-		go func(j int){
-			defer wg.Done()
-			address :="127.0.0.1:"+strconv.Itoa(j)
-				
-			conn,err := net.Dial("tcp",address)
-				if err != nil {
-					return
-				}
+func worker(ports chan int , wg *sync.WaitGroup){
+	for p:=range ports {
+		adress := fmt.Sprintf("google.com:%d",p)
+		conn,err := net.Dial("tcp",adress)
+		if err !=nil{
+			continue
+		}else{
+			fmt.Println("Connected to: ",adress)
 			conn.Close()
-			fmt.Printf("%d open\n",j)}(i)
+		}
+		
+		wg.Done()
+	}
+}
+func main()  {
+	ports := make(chan int,65535)
+	var wg sync.WaitGroup
+	for i:=0;i<cap(ports);i++{
+		go worker(ports,&wg)
+
+	}
+	for i:=1;i<=65535;i++{
+		wg.Add(1)
+		ports <- i 
 	}
 	wg.Wait()
+	close(ports)
+
 }
